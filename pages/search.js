3f8904c4eb20d06cfa2e1501e818baf2807f86aa
Vivek Yadav <enpvivek@gmail.com>
2022-07-22 08:20:54 +0530

    create about section of search result and created dynamic title for search page

--- pages/search.js
+++ pages/search.js
@@ -2,20 +2,25 @@ import Head from "next/head";
 import React from "react";
 //import { GetServerSideProps } from "next";
 import SearchHeader from "../components/SearchHeader";
+import SearchResults from "../components/SearchResults";
 import Response from "../Response";
+import { useRouter } from "next/router";
 
 const search = ({ results }) => {
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const router = useRouter();
   console.log(results);
   return (
     <div>
       <Head>
-        <title>Search Page</title>
+        <title>{router.query.term} G-search</title>
       </Head>
 
       {/* Search Header */}
       <SearchHeader />
 
       {/* Search Results */}
+      <SearchResults results={results} />
     </div>
   );
 };
